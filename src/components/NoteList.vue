19215250ebabae293cff6cd98281250188d944e3
MinhJay <minh.tu@ntq-solution.com.vn>
2019-03-26 14:01:06 +0700

    update base code

---
+++ src/components/NoteList.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="screen-box">
+    <header></header>
+    <main></main>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'note-list',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import NoteList from '@/components/NoteList'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/note-list',
+      name: 'note-list',
+      component: NoteList
     }
   ]
 })
