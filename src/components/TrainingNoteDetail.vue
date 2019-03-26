c6b3fea81f161e48f4c4b7f51b010bbc75f8c1aa
Yutaka Izumaru <y.izumaru@arrvis.com>
2019-03-26 18:55:08 +0900

    [add] component

---
+++ src/components/TrainingNoteDetail.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="training-note-detail">
+    <p>Training note detail</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import NoteList from '@/components/NoteList'
+import TrainingNoteDetail from '@/components/TrainingNoteDetail'
 
 Vue.use(Router)
 
@@ -18,6 +19,11 @@ export default new Router({
       path: '/note-list',
       name: 'note-list',
       component: NoteList
+    },
+    {
+      path: '/training-note-detail',
+      name: 'training-note-detail',
+      component: TrainingNoteDetail
     }
   ]
 })
