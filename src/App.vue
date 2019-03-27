324ff5808f77a33af6728c2cc55d9eb7765773ab
MinhJay <minh.tu@ntq-solution.com.vn>
2019-03-27 10:46:29 +0700

    update components base

--- src/App.vue
+++ src/App.vue
@@ -68,15 +68,14 @@ table {
 .item-box {
   border-radius: 5px;
   background: #fff;
-  box-shadow:
+  box-shadow: 0px 0px 5px 1px #eaeaea;
+  -moz-box-shadow: 0px 0px 5px 1px #eaeaea;
+  -webkit-box-shadow: 0px 0px 5px 1px #eaeaea;
 }
 
 .item-box__media {
   border-top-left-radius: 5px;
   border-top-right-radius: 5px;
-  box-shadow: 0px 0px 5px 1px #eaeaea;
-  -moz-box-shadow: 0px 0px 5px 1px #eaeaea;
-  -webkit-box-shadow: 0px 0px 5px 1px #eaeaea;
 }
 
 .item-box__content {
