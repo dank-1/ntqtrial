47bb0700acd8b26ed90d91bdc5afc575201adbdf
Yutaka Izumaru <y.izumaru@arrvis.com>
2019-03-27 16:14:58 +0900

    [add] eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: [
+    'plugin:vue/recommended'
+  ],
+  rules: {
+    "react/display-name": 1,
+    "semi" : [2 , "never"],
+    "key-spacing" : [2, {
+      "beforeColon" : true,
+      "afterColon" : true,
+    }],
+    "no-unused-vars": [1, {"vars" : "all", "args" : "after-used"} ],
+    "no-comma-dangle" : false
+  }
+}
