3d5f5178c4931424ee5c6bdc85f4f29798f29bfa
MinhJay <minh.tu@ntq-solution.com.vn>
2019-03-27 09:19:53 +0700

    integrate Vuex
    integrate Vuex

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+'use strict'
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
@@ -7,11 +7,10 @@ import store from './store';
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
   store,
+  router,
   template: '<App/>',
   components: { App }
 })
