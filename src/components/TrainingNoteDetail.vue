3acd7a48571418d8f6473fc14ae703452d695fb5
Yutaka Izumaru <y.izumaru@arrvis.com>
2019-03-27 16:45:20 +0900

    comment data

--- src/components/TrainingNoteDetail.vue
+++ src/components/TrainingNoteDetail.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="training-note-detail">
-    <p>Training note detail</p>
   </div>
 </template>
 
@@ -14,3 +13,14 @@ export default {
 </script>
 
 <style></style>
+// {
+//   "code": 200,
+//   "data": {
+//     "trainingNote": {
+//       "id": 1,
+//       "videoUrl": "http://sample.com/vidoe_sample.mp4",
+//       "thumbnailUrl": "http://sample.com/thumbnail_sample.png",
+//       "trainingSeconds": 5400
+//     }
+//   }
+// }
