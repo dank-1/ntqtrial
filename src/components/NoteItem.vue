8fb6c5c022ae578748d8b6ced2705255d2c5bc43
Binh Vu Van <binh.vu@ntq-solution.com.vn>
2019-03-27 09:50:01 +0700

    Note List

---
+++ src/components/NoteItem.vue
@@ -0,0 +1,69 @@
+<template>
+  <section class="item-box item-note-list">
+    <div class="item-box__media">
+      <img v-bind:src="noteItem.image" v-bind:alt="noteItem.title" />
+    </div>
+    <div class="item-box__content">
+      <span class='title'> {{ noteItem.title }}</span>
+      <div class='description'>
+          {{ noteItem.description }}
+      </div>
+      <span class='created-date'> {{ noteItem.createDate }}</span>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'note-item',
+  props: {
+    noteItem: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style>
+
+.item-note-list{
+  width: 90%;
+  margin:20px auto;
+  font-size: 14px;
+}
+
+.item-note-list{
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+  box-shadow: 0px 0px 5px 1px #eaeaea;
+  -moz-box-shadow: 0px 0px 5px 1px #eaeaea;
+  -webkit-box-shadow: 0px 0px 5px 1px #eaeaea;
+}
+
+.item-note-list .item-box__media img {
+  width:100%;
+  height:180px;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+}
+
+.item-note-list .item-box__content span.title {
+  display:inline-block;
+  background: #c5d219;
+  border-radius: 5px;
+  padding:4px;
+  color: black;
+  font-weight: bold;
+}
+
+.item-note-list .item-box__content .description {
+  margin: 20px auto;
+  color: black;
+}
+
+.item-note-list .item-box__content span.created-date {
+  color: #727375;
+}
+
+</style>
--- src/components/NoteList.vue
+++ src/components/NoteList.vue
@@ -1,37 +1,21 @@
 <template>
   <section class="screen-box">
-    <header>
-      <div class='header'>
-        <span class='icon-back' @click="handleBack('dantri.com.vn')"></span>
-        <span class='title'> リーフォーマッ </span>
-      </div>
-    </header>
+    <header-note title="リーフォーマッ" url="http://dantri.com.vn"></header-note>
     <main>
-      <section class="item-box item-note-list" v-for="itemNoteList in noteList">
-        <div class="item-box__media">
-          <img v-bind:src="itemNoteList.image" v-bind:alt="itemNoteList.title" />
-        </div>
-        <div class="item-box__content">
-          <span class='title'> {{ itemNoteList.title }}</span>
-          <div class='description'>
-             {{ itemNoteList.description }}
-          </div>
-          <span class='created-date'> {{ itemNoteList.createDate }}</span>
-        </div>
-      </section>
-
+      <template v-for="itemNoteList in noteList">
+        <note-item :noteItem="itemNoteList"></note-item>
+      </template>
       <button class='submit-note-list'>練習ノート</button>
     </main>
   </section>
 </template>
 
 <script>
-// import Header from '@./Header'
 export default {
   name: 'note-list',
   data () {
     return {
-        noteList: [
+      noteList: [
           { title: 'カリキュラムタイトル', image: './static/img/student.jpg', description: 'フリーフォーマットでのテキストフリーフォーマットでのテキストフリーフォーマットでのテキストフリーフォーマットでのテキストフリーフォーマットでのテキストフリーフォーマットでのテキストフリーフォーマットでのテキスト', createDate: '2018.02.10' },
           { title: 'カリキュラムタイトル', image: './static/img/student.jpg', description: 'フリーフォーマットでのテキストフリーフォーマットでのテキストフリーフォーマットでのテキストフリーフォーマットでのテキストフリーフォーマットでのテキストフリーフォーマットでのテキストフリーフォーマットでのテキスト', createDate: '2018.02.10' },
           { title: 'カリキュラムタイトル', image: './static/img/student.jpg', description: 'フリーフォーマットでのテキストフリーフォーマットでのテキストフリーフォーマットでのテキストフリーフォーマットでのテキストフリーフォーマットでのテキストフリーフォーマットでのテキストフリーフォーマットでのテキスト', createDate: '2018.02.10' },
@@ -39,112 +23,45 @@ export default {
       ]
     }
   },
-
-  methods: {
-    /**
-      * Back Url
-      * @param url
-      *
-      * @function toggleSelect
-      */
-
-    handleBack(url) {
-        this.$router.push(url);
-    },
-  }
 }
 </script>
 
 <style>
-.header {
-  width: 90%;
-  margin:5px auto;
-  margin-top:20px;
-  height: 30px;
-}
-.header .icon-back{
-  background-image: '/static/img/icons/icon_back.png';
-  background:red;
-  width:10%;
-  height:30px;
-  display:block;
-  float: left;
-}
-
-.header .title {
-  width:80%;
-  height:30px;
-  display:block;
-  float: left;
-  text-align: center;
-  line-height: 30px;
-  font-weight: bold;
-}
-
-.item-note-list{
-  width: 90%;
-  margin:20px auto;
-  font-size: 14px;
-}
-
-.item-note-list{
-  border-top-left-radius: 5px;
-  border-top-right-radius: 5px;
-  box-shadow: 0px 0px 5px 1px #eaeaea;
-  -moz-box-shadow: 0px 0px 5px 1px #eaeaea;
-  -webkit-box-shadow: 0px 0px 5px 1px #eaeaea;
-}
-
-.item-note-list .item-box__media img {
-  width:100%;
-  height:180px;
-  border-top-left-radius: 5px;
-  border-top-right-radius: 5px;
-}
-
-.item-note-list .item-box__content span.title {
-  display:inline-block;
-  background: #c5d219;
-  border-radius: 5px;
-  padding:4px;
-  color: black;
-  font-weight: bold;
-}
-
-.item-note-list .item-box__content .description {
-  margin: 20px auto;
-  color: black;
-}
-
-.item-note-list .item-box__content span.created-date {
-  color: #727375;
-}
-
 .submit-note-list {
-    width:80%;
-    margin: 10px auto;
-    display: inline-block;
-    margin-bottom: 0;
-    font-weight: 400;
-    text-align: center;
-    white-space: nowrap;
-    vertical-align: middle;
-    -ms-touch-action: manipulation;
-    touch-action: manipulation;
-    cursor: pointer;
-    background-image: none;
-    border: 1px solid transparent;
-    padding: 6px 12px;
-    font-size: 14px;
-    line-height: 1.42857143;
-    border-radius: 4px;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-    color: #fff;
-    background-color: #e4327d;
-    border-color: #d43f3a;
+  width:80%;
+  margin: 10px auto;
+  display: inline-block;
+  margin-bottom: 0;
+  font-weight: 400;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: middle;
+  -ms-touch-action: manipulation;
+  touch-action: manipulation;
+  cursor: pointer;
+  background-image: none;
+  border: 1px solid transparent;
+  padding: 6px 12px;
+  font-size: 14px;
+  line-height: 1.42857143;
+  border-radius: 4px;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  color: #fff;
+  background-color: #e4327d;
+  border-color: #d43f3a;
+  position: fixed;
+  overflow: hidden;
+  z-index: 2400;
+  right: 35px;
+  bottom: 50px !important;
+  -webkit-transition: all 0.5s ease-in-out;
+  -moz-transition: all 0.5s ease-in-out;
+  -ms-transition: all 0.5s ease-in-out;
+  -o-transition: all 0.5s ease-in-out;
+  transition: all 0.5s ease-in-out;
 }
 
 </style>
