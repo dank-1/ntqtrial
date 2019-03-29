08e3a732c7f9b010cae0563f76f1bf8437b6e6dc
MinhJay <minh.tu@ntq-solution.com.vn>
2019-03-29 12:38:57 +0700

    Merge conflicts

---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="video-player">
+    <template v-if="!playingUrl">
+      <img :src="thumbnailUrl" @click="onClickVideo(videoUrl)" :style="styleObject" />
+      <span class='icon-play' @click="onClickVideo(videoUrl)"></span>
+    </template>
+    <video
+      v-show="playingUrl"
+      ref="video"
+      :src="playingUrl"
+      controls=true
+      autoplay=true
+      type="video/mp4" />
+  </div>
+</template>
+
+<script>
+const purposeTypes = {
+  curriculumOverview: { width: '100%' },
+  curriculumStep: { width: '100%', 'border-radius': '8px' }
+}
+
+export default {
+  name: 'video-player',
+  props: {
+    purpose: {
+      default: 'curriculumOverview',
+      validator: function(value) {
+        return Object.keys(purposeTypes).indexOf(value) !== -1
+      }
+    },
+    videoUrl: { type: String, required: true },
+    thumbnailUrl: { type: String, required: true }
+  },
+  data () {
+    return {
+      playingUrl: null
+    }
+  },
+  computed: {
+    styleObject () {
+      return purposeTypes[this.purpose]
+    }
+  },
+  methods: {
+    onClickVideo (url) {
+      this.playingUrl = url
+      this.$refs['video'].play()
+      this.requestFullScreen()
+    },
+    requestFullScreen () {
+      const elem = this.$refs['video']
+      if ( elem.webkitRequestFullScreen ) {
+        elem.webkitRequestFullScreen();
+        elem.addEventListener('webkitfullscreenchange', this.changeFullScreen)
+      }
+    },
+    changeFullScreen (e) {
+      if (document.webkitFullscreenElement === null) {
+        this.$refs['video'].pause()
+        this.playingUrl = null
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.video-player {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.video-player {
+  position: relative;
+}
+
+.video-player .icon-play {
+  background: url("/static/img/icons/icon-play.png") no-repeat;
+  background-position: center center;
+  background-size:contain;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 64px;
+  height: 64px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import NoteList from '@/components/NoteList'
+import Curriculum from '@/components/Curriculum'
 import Live from '@/components/Live'
 import Feed from '@/components/Feed'
 import NoteCreate from '@/components/NoteCreate'
@@ -21,6 +22,11 @@ export default new Router({
       path: '/note-list',
       name: 'note-list',
       component: NoteList
+    },
+    {
+      path: '/curriculum/:curriculumId',
+      name: 'curriculum',
+      component: Curriculum
     },
     {
       path: '/live',
